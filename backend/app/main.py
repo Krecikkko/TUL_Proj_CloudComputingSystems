a05b663beafb16cc9cccfb412e360dddd1f1652a
lauragabryjanczyk <laura@ilex.net.pl>
2025-11-19 14:47:09 +0100

    Last week's tasks completed: admin user management: implemented /api/admin router, enabling administrators to list all users, delete accounts, and update user roles; user statistics added (/api/users/stats endpoint); redis-based  rate limiting added

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,16 +5,38 @@ from .routes import (
     auth as auth_router,
     files as files_router,
     log as logbook_router,
-    share as share_router
+    share as share_router,
+    admin as admin_router
 )
 from .db import init_db
 from contextlib import asynccontextmanager
+import redis.asyncio as redis 
+from fastapi_limiter import FastAPILimiter 
+import os 
+
+
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Creating database and tables...")
     await init_db()
+    
+    # === Rate Limiting Initialization ===
+    print(f"Initializing Rate Limiter with Redis at {REDIS_URL}...")
+    try:
+        redis_connection = redis.from_url(REDIS_URL, encoding="utf8", decode_responses=True)
+        await FastAPILimiter.init(redis_connection)
+    except Exception as e:
+        print(f"INITIALIZATION ERROR REDIS/FASTAPILIMITER: {e}")
+        
     yield
+    # === Rate Limiter Cleanup ===
+    try:
+        await FastAPILimiter.close() 
+    except Exception:
+        pass 
+        
     print("Application shutdown.")
 
 app = FastAPI(lifespan=lifespan)
@@ -26,6 +48,7 @@ app.include_router(files_router.router)
 app.include_router(fileversion_router.router)
 app.include_router(logbook_router.router)
 app.include_router(share_router.router)
+app.include_router(admin_router.router)
 
 @app.get("/api")
 def root():
